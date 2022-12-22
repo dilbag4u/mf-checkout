15523c45e51678ceafce4d952b40790260eef3e6
Dilbag <dilbag.singh@matchesfashion.com>
2022-12-22 18:33:37 +0000

    cucumber js file

--- cucumber.js
+++ cucumber.js
@@ -2,7 +2,7 @@
 module.exports = {
     default: {
       paths: ["tests/login.feature"],
-      import: ["steps/"],
+      import: ["steps/login-def.mjs"],
       publishQuiet: true,
       
     }
